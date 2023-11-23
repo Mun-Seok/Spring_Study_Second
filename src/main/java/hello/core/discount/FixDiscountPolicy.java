329196f8d339ae7e7e8e6e33aa6b37bdbac7b0f3
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-23 21:47:51 +0900

    Order and Discount Policy

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; // 1000원 할인
+    @Override
+    public int discount(Member member, int price) {
+        // VIP면 1000원 할인 나머지는 0원
+        if (member.getGrade() == Grade.VIP) {
+           return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}

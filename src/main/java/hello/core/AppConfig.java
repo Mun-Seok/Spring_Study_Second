4900e4485c0af1a060c511ef9d23840ae561e062
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-24 17:45:24 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,20 @@ import hello.core.order.OrderServiceImpl;
 
 // 실제 동작에 필요한 구현 객체를 생성하고 생성자를 통해서 주입한다.
 public class AppConfig {
-
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }

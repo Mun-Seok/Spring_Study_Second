f7d784c50d61ee36307f8898fb2257756bd98ca8
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-28 17:57:52 +0900

    ComponentScan designate

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 // 컴포넌트 스캔 의존관계 자동 주입하기
 @Configuration
 @ComponentScan( // 컴포너트 스캔 @Component 애노테이션 붙은 클래스 스캔해서 스프링 빈으로 등록함
+        basePackages = "hello.core", // 지정한 패키지부터 하위 패키지 찾음
+        basePackageClasses = AutoAppConfig.class, // 지정 클래스 패키지 탐색 위치
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class) // Configuration 제외
 )
 
